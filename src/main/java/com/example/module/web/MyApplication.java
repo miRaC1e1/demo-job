c37263836fc149f5c5001aa30739579fd454b5a4
huiqing.hu <huiqing.hu@envision-energy.com>
2022-09-02 09:54:50 +0800

    springboot环境配置

---
+++ src/main/java/com/example/module/web/MyApplication.java
@@ -0,0 +1,27 @@
+package com.example.module.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * MyApplication
+ *
+ * @author Envision-huiqing.hu
+ * @since 2022/9/1
+ */
+@RestController
+@EnableAutoConfiguration
+public class MyApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+}
